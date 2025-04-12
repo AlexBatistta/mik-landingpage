708c919338e41106f9f36358441508a058670e69
Alex Batistta <batistta.alex@outlook.com>
2025-04-12 10:11:27 -0300

    brand setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 function App() {
   return (
-    <>
-    
-    </>
+    <main className="flex flex-col items-center justify-center min-h-screen bg-orange-secondary">
+      <h1 className="text-green-primary font-bropella">TITULO</h1>
+      <p className="text-green-secondary font-century-gothic">Parrafo</p>
+      <p className="text-green-secondary font-century-gothic-bold">Parrafo bold</p>
+    </main>
   )
 }
 
